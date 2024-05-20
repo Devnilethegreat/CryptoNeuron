1752a32dc35b41018a9db76bfcaa2b005de6cc7d
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2024-05-20 16:34:58 +0000

    Scaffold core module structure

---
+++ cryptoneuron.py
@@ -0,0 +1,14 @@
+# cryptoneuron.py
+"""
+Main module for CryptoNeuron application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
